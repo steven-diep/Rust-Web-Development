b49296e3517b49541c608a5eb36a998df1b12a12
steven-diep <steven387988@gmail.com>
2024-04-20 16:37:32 -0700

    Added server code

--- rustproject/src/main.rs
+++ rustproject/src/main.rs
@@ -1,5 +1,7 @@
 mod question;
 
+use axum::{routing::get, Router};
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 use question::*;
 
 #[tokio::main]
@@ -11,4 +13,14 @@ async fn main() {
         Some(vec!("faq".to_string()))
     );
     println!("{:?}", question);
+
+    // Create an app
+    let hello = Router::new().route("/", get("Hello, World!"));
+
+    // Host the app on localhost:3000
+    let ip = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
+
+    // Run the app
+    let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
+    axum::serve(listener, hello).await.unwrap();
 }
