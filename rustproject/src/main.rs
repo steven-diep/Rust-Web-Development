6461364acd9ae7693fc7f28cdaf567f9a2a14420
steven-diep <steven387988@gmail.com>
2024-04-19 20:47:15 -0700

    Created a basic question struct

--- rustproject/src/main.rs
+++ rustproject/src/main.rs
@@ -1,15 +1,14 @@
-use axum::{routing::get, Router};
-use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+mod question;
+
+use question::*;
 
 #[tokio::main]
 async fn main() {
-    // Create an app
-    let hello = Router::new().route("/", get("Hello, World!"));
-
-    // Host the app on localhost:3000
-    let ip = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
-
-    // Run the app
-    let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
-    axum::serve(listener, hello).await.unwrap();
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string()))
+    );
+    println!("{:?}", question);
 }
