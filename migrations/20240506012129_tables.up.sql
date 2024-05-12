848579a20e4526639fe1a49117d39ec1c3f55fb8
steven-diep <steven387988@gmail.com>
2024-05-12 14:50:07 -0700

    fixed migration files, they were backwards

--- migrations/20240506012129_tables.up.sql
+++ migrations/20240506012129_tables.up.sql
@@ -1,3 +1,25 @@
 -- Add up migration script here
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS answers;
+CREATE TABLE IF NOT EXISTS questions (
+  id serial PRIMARY KEY,
+  title VARCHAR (255) NOT NULL,
+  content TEXT NOT NULL,
+  tags TEXT [],
+  created_on TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO questions (title, content, tags)
+VALUES ('Sample Question 1', 'Sample Content 1', ARRAY['Sample', 'Content']);
+
+INSERT INTO questions (title, content, tags)
+VALUES ('Sample Question 2', 'Sample Content 2', ARRAY['Example', 'Content']);
+
+INSERT INTO questions (title, content, tags)
+VALUES ('Sample Question 3', 'Sample Content 3', ARRAY['FAQ']);
+
+
+CREATE TABLE IF NOT EXISTS answers (
+  id serial PRIMARY KEY,
+  content TEXT NOT NULL,
+  created_on TIMESTAMP NOT NULL DEFAULT NOW(),
+  corresponding_question INTEGER REFERENCES questions(id)
+);
