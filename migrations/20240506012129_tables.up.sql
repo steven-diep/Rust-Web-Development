56e094640a81ac4fc30fb0ff0848620653dfb723
steven-diep <steven387988@gmail.com>
2024-05-19 15:18:34 -0700

    added cascade to db to delete answers when question is deleted

--- migrations/20240506012129_tables.up.sql
+++ migrations/20240506012129_tables.up.sql
@@ -21,5 +21,5 @@ CREATE TABLE IF NOT EXISTS answers (
   id serial PRIMARY KEY,
   content TEXT NOT NULL,
   created_on TIMESTAMP NOT NULL DEFAULT NOW(),
-  corresponding_question INTEGER REFERENCES questions(id)
+  corresponding_question INTEGER REFERENCES questions(id) ON DELETE CASCADE
 );
